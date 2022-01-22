3fed9812bd7e1b279212d61e60212d07d26f27e4
Brandan <brandan.jones@uc.edu>
2022-01-22 13:13:46 -0500

    Add unit test for 3 + 3 = 6

--- app/src/test/java/app/plantdiary/ExampleUnitTest.kt
+++ app/src/test/java/app/plantdiary/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun threePlusThree_EqualsSix() {
+        assertEquals(6, 3+3)
+    }
 }
