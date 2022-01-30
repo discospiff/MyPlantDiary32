307ad9513c344214f3a7e2e495ce5028d9cee8c8
Brandan <brandan.jones@uc.edu>
2022-01-30 17:40:19 -0500

    Create Simple Compose UI with OutlinedTextField, Button, Toast. See demonstration in this video: https://youtu.be/YhimOEoZT_8

--- app/src/main/java/app/plantdiary/MainActivity.kt
+++ app/src/main/java/app/plantdiary/MainActivity.kt
@@ -1,12 +1,15 @@
 package app.plantdiary
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import app.plantdiary.ui.theme.MyPlantDiaryTheme
 
@@ -17,7 +20,7 @@ class MainActivity : ComponentActivity() {
             MyPlantDiaryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    SpecimenFacts("Android")
                 }
             }
         }
@@ -25,14 +28,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SpecimenFacts(name: String) {
+    var plantName by remember { mutableStateOf("")}
+    var location by remember { mutableStateOf("")}
+    var description by remember { mutableStateOf("")}
+    var datePlanted by remember { mutableStateOf("")}
+    val context = LocalContext.current
+    Column {
+        OutlinedTextField(
+            value = plantName,
+            onValueChange = {plantName = it},
+            label = { Text(stringResource(R.string.plantName))}
+            )
+        OutlinedTextField(
+            value = location,
+            onValueChange = {location = it},
+            label = { Text(stringResource(R.string.location))}
+        )
+        OutlinedTextField(
+            value = description,
+            onValueChange = {description = it},
+            label = { Text(stringResource(R.string.description))}
+        )
+        OutlinedTextField(
+            value = datePlanted,
+            onValueChange = {datePlanted = it},
+            label = { Text(stringResource(R.string.datePlanted))}
+        )
+        Button (
+            onClick = {
+                Toast.makeText(context, "$plantName $location $description $datePlanted", Toast.LENGTH_LONG).show()
+            }
+        ){
+            Text(text = "Save")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyPlantDiaryTheme {
-        Greeting("Android")
+        SpecimenFacts("Android")
     }
 }
