df8802319bc70febab4ddacf0d0ad46daee38d69
Brandan <brandan.jones@uc.edu>
2022-01-30 21:11:26 -0500

    Add color themes, change title bar color, implement modifiers for sizing, and implement preview. See demonstration in these videos: https://youtu.be/_WQ3Ocl1TJA https://youtu.be/nw35zroX3AQ

--- app/src/main/java/app/plantdiary/MainActivity.kt
+++ app/src/main/java/app/plantdiary/MainActivity.kt
@@ -1,13 +1,16 @@
 package app.plantdiary
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +22,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyPlantDiaryTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.background,
+                modifier = Modifier.fillMaxWidth()) {
                     SpecimenFacts("Android")
                 }
             }
@@ -38,22 +42,26 @@ fun SpecimenFacts(name: String) {
         OutlinedTextField(
             value = plantName,
             onValueChange = {plantName = it},
-            label = { Text(stringResource(R.string.plantName))}
+            label = { Text(stringResource(R.string.plantName))},
+            modifier = Modifier.fillMaxWidth()
             )
         OutlinedTextField(
             value = location,
             onValueChange = {location = it},
-            label = { Text(stringResource(R.string.location))}
+            label = { Text(stringResource(R.string.location))},
+            modifier = Modifier.fillMaxWidth()
         )
         OutlinedTextField(
             value = description,
             onValueChange = {description = it},
-            label = { Text(stringResource(R.string.description))}
+            label = { Text(stringResource(R.string.description))},
+            modifier = Modifier.fillMaxWidth()
         )
         OutlinedTextField(
             value = datePlanted,
             onValueChange = {datePlanted = it},
-            label = { Text(stringResource(R.string.datePlanted))}
+            label = { Text(stringResource(R.string.datePlanted))},
+            modifier = Modifier.fillMaxWidth()
         )
         Button (
             onClick = {
@@ -65,10 +73,15 @@ fun SpecimenFacts(name: String) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name="Light Mode", showBackground=true)
+@Preview(uiMode= Configuration.UI_MODE_NIGHT_YES, showBackground = true, name="Dark Mode")
 @Composable
 fun DefaultPreview() {
     MyPlantDiaryTheme {
-        SpecimenFacts("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxWidth()) {
+            SpecimenFacts("Android")
+        }
     }
 }
