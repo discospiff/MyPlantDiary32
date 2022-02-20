b439179eab064f9c63e6aa1a3e732e238c242370
Brandan <brandan.jones@uc.edu>
2022-02-19 19:18:52 -0500

    Integrate Firebase into our project. See demonstration in this video: https://youtu.be/tC714ewbWa4

--- app/src/main/java/app/plantdiary/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/MainViewModel.kt
@@ -6,12 +6,14 @@ import androidx.lifecycle.viewModelScope
 import app.plantdiary.dto.Plant
 import app.plantdiary.service.IPlantService
 import app.plantdiary.service.PlantService
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 
 class MainViewModel(var plantService : IPlantService =  PlantService()) : ViewModel() {
 
     var plants : MutableLiveData<List<Plant>> = MutableLiveData<List<Plant>>()
 
+    private lateinit var firestore : FirebaseFirestore
 
     fun fetchPlants() {
         viewModelScope.launch {
